00695c09e2cfd85efeb5ad9d0d9d89412212aaac
Veera Sankar <sankarsivam6991@gmail.com>
2025-12-08 11:56:54 +0530

    Handling Undefined Elements

--- assets/js/array/arrayDestructuring.js
+++ assets/js/array/arrayDestructuring.js
@@ -2,9 +2,9 @@
 
 // Method:1
 const subjectMarks = [50, 70, 95];
-console.log("physics", subjectMarks[0]); //50
-console.log("chemistry", subjectMarks[1]); //70
-console.log("maths", subjectMarks[2]); //95
+console.log("physics", subjectMarks[0]); //physics 50
+console.log("chemistry", subjectMarks[1]); //chemistry 70
+console.log("maths", subjectMarks[2]); //maths 95
 
 // Method:2
 const subMarks = [65, 75, 85];
@@ -13,13 +13,18 @@ let botany = subMarks[0];
 let zoology = subMarks[1];
 let evs = subMarks[2];
 
-console.log("botany", botany);
-console.log("zoology", zoology);
-console.log("evs", evs);
+console.log("botany", botany); //botany 65
+console.log("zoology", zoology); //zoology 75
+console.log("evs", evs); //evs 85
 
 // Method:3
 const [Tamil, English, Economy] = [80, 90, 100];
 
-console.log("Tamil", Tamil);
-console.log("English", English);
-console.log("Economy", Economy);
+console.log("Tamil", Tamil); //Tamil 80
+console.log("English", English); //English 90
+console.log("Economy", Economy); //Economy 100
+
+// Handling Undefined Elements
+const [Tam, Eng, Economics] = [80, 90];
+
+console.log(Tam, Eng, Economics); //80 90 undefined
